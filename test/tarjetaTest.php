102e013e253ca6f53d787cd0f482d1b4747a1e00
cristianquintaa <crisslipknot98@gmail.com>
2016-09-29 00:21:01 -0300

    Update tarjetaTest.php

--- test/tarjetaTest.php
+++ test/tarjetaTest.php
@@ -9,7 +9,7 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
 			$this->tarjeta = new Baja();
 			$this->colectivo1 = new Colectivo("144 Negro", "Rosario Bus");}	
 
-  public function testCargaSaldo() {
+  public function testRecargar() {
     $this->tarjeta->recargar(272);
     $this->assertEquals($this->tarjeta->saldo(), 320, "Cuando cargo 272 deberia tener finalmente 320");
     $this->tarjeta = new Baja();
@@ -17,7 +17,7 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
     $this->assertEquals($this->tarjeta->saldo(), 640, "Cuando cargo 500 deberia tener finalmente 640");
   }
 
- public function testPagarViaje() {
+ public function testPagar() {
   	$this->tarjeta->recargar(272);
   	$this->tarjeta->pagar($this->colectivo1, "2016/09/28 23:51");
   	$this->assertEquals($this->tarjeta->saldo(), 312, "Cuando recargo 272 y pago un colectivo deberia tener finalmente 312");
