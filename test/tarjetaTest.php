dbc70b13d629b9663a78c80d319e3ce593f3262f
cristianquitaa <crisslipknot98@gmail.com>
2016-10-15 19:13:44 -0300

    Añadiendo transbordo al test

--- test/tarjetaTest.php
+++ test/tarjetaTest.php
@@ -18,21 +18,20 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals($this->tarjeta->saldo(), 640, "Cuando cargo 500 deberia tener finalmente 640");
 	}
 	public function testPagar() {
+		$this->tarjeta = new Baja();
 		$this->tarjeta->recargar(272);
   		$this->tarjeta->pagar($this->colectivo1, "2016/09/28 23:51");
   		$this->assertEquals($this->tarjeta->saldo(), 312, "Cuando recargo 272 y pago un colectivo deberia tener finalmente 312");
   	}
-	public function testNombreColectivo(){
-		$this->assertEquals($this->colectivo1->Nombre(), "144 Negro", "Cuando pregunto por el nombre del colectivo debería decir 144 Negro");
-		
-	}
 	public function testTransbordo(){
+		$this->tarjeta = new Baja();
 		$this->tarjeta->recargar(100);
 		$this->tarjeta->pagar($this->colectivo1, "2016/09/28 19:04");
 		$this->tarjeta->pagar($this->colectivo2, "2016/09/28 19:30");
 		$this->assertEquals($this->tarjeta->saldo(), 89,36, "Si cargo 100 y pago dos colectivos y uno con transbordo deberia tener 89,36");
 	}
 	public function testSinTransbordo(){
+		$this->tarjeta = new Baja();
 		$this->tarjeta->recargar(100);
 		$this->tarjeta->pagar($this->colectivo1, "2016/09/28 19:04");
 		$this->tarjeta->pagar($this->colectivo2, "2016/09/28 20:05");
