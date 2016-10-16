553ebac64470c8dd685da11b8592b3e09c258e20
cristianquitaa <crisslipknot98@gmail.com>
2016-10-15 21:43:14 -0300

    Agregando testBici

--- test/tarjetaTest.php
+++ test/tarjetaTest.php
@@ -10,6 +10,7 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
 		$this->tarjeta2 = new Baja();
 		$this->colectivo1 = new Colectivo("144 Negro", "Rosario Bus");
 		$this->colectivo2 = new Colectivo("142 Rojo", "Rosario Bus");
+		$this->bici = new Bici(666);
 	}
 	public function testRecargar() {
 		$this->tarjeta->recargar(272);
@@ -36,6 +37,11 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
 		$this->tarjeta2->pagar($this->colectivo1, "2016/09/10 19:04");
 		$this->tarjeta2->pagar($this->colectivo2, "2016/09/10 20:30");
 		$this->assertEquals($this->tarjeta2->saldo(), 304, "Si cargo 272 y pago dos colectivos sin transbordo deberia tener 304");
+	}
+	public function testPagarBici(){
+		$this->tarjeta2->recargar(272);
+		$this->tarjeta2->pagar($this->bici,"2016/09/10 12:00");
+		$this->assertEquals($this->tarjeta2->saldo(), 308, "Si cargo 272 y pago dos colectivos sin transbordo deberia tener 308");
 	}
 }
 ?>
