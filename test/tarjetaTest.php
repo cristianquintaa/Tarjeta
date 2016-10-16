63a06dd2dd4f7782e2b4b997c1026bd07120ff63
cristianquitaa <crisslipknot98@gmail.com>
2016-10-15 21:02:32 -0300

    Arreglando TestSinTransbordo

--- test/tarjetaTest.php
+++ test/tarjetaTest.php
@@ -32,7 +32,9 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
 	}
 	public function testSinTransbordo(){
 		$this->tarjeta = new Baja();
+		$this->assertEquals($this->tarjeta->saldo(), 0, "Si cargo 100 y pago dos colectivos sin transbordo deberia tener 84");
 		$this->tarjeta->recargar(100);
+		$this->assertEquals($this->tarjeta->saldo(), 100, "Si cargo 100 y pago dos colectivos sin transbordo deberia tener 84");
 		$this->tarjeta->pagar($this->colectivo1, "2016/09/10 19:04");
 		$this->assertEquals($this->tarjeta->saldo(), 92, "Si cargo 100 y pago dos colectivos sin transbordo deberia tener 84");
 		$this->tarjeta->pagar($this->colectivo2, "2016/09/10 20:30");
