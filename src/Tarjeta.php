d859fc80e85758abce52c9a85b1da54fed10f4cb
cristianquitaa <crisslipknot98@gmail.com>
2016-10-16 15:39:05 -0300

    Borre una funcion en Baja y Tarjeta para llegar al 100%  :D

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -7,6 +7,5 @@ interface Tarjeta {
 	public function pagar(Transporte $transporte, $fecha_y_hora);
 	public function recargar($monto);
 	public function saldo();
-	public function viajesRealizados();
 }
 
