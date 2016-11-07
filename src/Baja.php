220347f063c7acde17fd28f0ca559fc128de6bbf
cristianquitaa <crisslipknot98@gmail.com>
2016-11-07 19:47:25 -0300

    Arreglando errores

--- src/Baja.php
+++ src/Baja.php
@@ -14,15 +14,13 @@ class Baja implements Tarjeta {
     $this->lunes = strtotime("2016/01/04 00:00");
   }
   public function pagar(Transporte $transporte, $fecha_y_hora){
-    $aux1=strtotime($fecha_y_hora);
-    $aux2=strtotime($this->ultimafecha);
-    $dia = $aux1 - $this->lunes;
-	$a = $dia % 86400;
-	$dia = $dia - $a;
-	$dia = ($dia/86400) % 7;
-
-    if ($transporte->tipo() == "colectivo"){
-      $trasbordo = false;
+	if ($transporte->tipo() == "colectivo"){
+      $aux1=strtotime($fecha_y_hora);
+   	  $aux2=strtotime($this->ultimafecha);
+	  $dia = $aux1 - $this->lunes;
+      $a = $dia % 86400;
+	  $dia = $dia - $a;
+	  $dia = ($dia/86400) % 7;
       if(($dia==5 && $a>50400 && $a<79200) || ($dia==6 && $a>21600 && $a<79200) || $a<21600 || $a>79200 ){
       	$this->tiempomaxtransbordo=5400;
       } 
@@ -30,11 +28,13 @@ class Baja implements Tarjeta {
       	$this->tiempomaxtransbordo=3600;
       }
 
-      if (count($this->viajes) > 0){
-        if($this->ultimafecha == 0 || ($aux1-$aux2>$this->tiempomaxtransbordo) || $this->viajes[$this->ultimafecha]->getTransporte()->tipo() == $transporte->tipo()){
-          $trasbordo = true;
+      if($this->ultimafecha == 0 || ($aux1-$aux2>$this->tiempomaxtransbordo) || $this->viajes[$this->ultimafecha]->getTransporte()->tipo() == $transporte->tipo()){
+          $trasbordo = false;
         }
-      }
+       else{
+       	$trasbordo = true;
+       }
+      
       $monto = 0;
       if ($trasbordo){
         $monto = 2.5*$this->descuento;
@@ -59,27 +59,26 @@ class Baja implements Tarjeta {
       	$this->viajes[] = new Viaje($transporte->tipo(), $monto, $transporte, strtotime($fecha_y_hora));
       }
     } 
-    else {
-      if ($transporte->tipo() == "bici"){
+    if ($transporte->tipo() == "bici"){
       	$aux1 = strtotime($fecha_y_hora);
 		$aux2 = strtotime($this->ultimabicipaga);
 		if($this->ultimabicipaga == 0 || ($aux1-$aux2>86400)){
 			$this->saldo = $this->saldo-12;
         	if($this->saldo <0){
          	 $this->saldo= $this->saldo +12;
-			$this->ultimabicipaga = $fecha_y_hora;
 			} 
 			else{
 				$this->viajes[] = new Viaje($transporte->tipo(), 12, $transporte, strtotime($fecha_y_hora));
+				$this->ultimabicipaga = $fecha_y_hora;
 			}
 		}
 		else {
 			$this->viajes[] = new Viaje($transporte->tipo(), 12, $transporte, strtotime($fecha_y_hora));
+			$this->ultimabicipaga = $fecha_y_hora;
 		}
         
     }   
-  }
-}
+} 
   public function recargar($monto){
     if ($monto == 272){
       $this->saldo = $this->saldo + 320;
